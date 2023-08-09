ce17a7256b0324c303cb3eeeb20f37e601f6ce81
achraflab <labidiachraf6@gmail.com>
2023-08-09 11:05:22 +0200

    Project setup

---
+++ src/main/java/com/darklab/asteroids/AsteroidsApplication.java
@@ -0,0 +1,13 @@
+package com.darklab.asteroids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteroidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsteroidsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darklab/asteroids/AsteroidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darklab.asteroids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsteroidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
